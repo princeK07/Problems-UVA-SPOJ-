c21fac5f639b3bfb06b973d1b4fd46e6315753e1
l3vel <109853601+princeK07@users.noreply.github.com>
2022-08-07 14:28:24 +0530

    Create (SPOJ)-EKO - Eko.cpp

---
+++ (SPOJ)-EKO - Eko.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define clr(a) memset((a),0,sizeof(a))
+
+typedef long long ll;
+
+const int N = 1e6+10;
+ll n, m;
+ll trees[N];
+
+bool valid(int value){
+  ll total=0;
+  for(int i=0; i<n; i++){
+    if(trees[i]>=value)
+      total+=(trees[i]-value);
+  }
+  return total>=m;
+}
+
+void solve(){
+  cin>>n>>m;
+  clr(trees);
+
+  for(int i=0; i<n; i++) cin>>trees[i];            
+
+  ll lo=0, hi=1e9, mid;
+  
+  while(hi-lo>1){
+    mid = (lo+hi)/2;
+    if(valid(mid)) lo=mid;
+    else hi=mid-1;    
+  }
+  if(valid(hi)) cout<<hi;
+  else cout<<lo;
+}
+
+int main() {
+  #ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+  #endif
+
+  ios_base::sync_with_stdio(false);
+  cout.tie(nullptr);
+
+  int t=1;
+  // cin>>t;
+  while(t--){
+    solve();
+    // cout<<nl;
+  }
+
+}
