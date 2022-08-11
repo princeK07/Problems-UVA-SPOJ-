a4358fbc08a1d12fd1148759ba20bf5b14b91ed8
l3vel <109853601+princeK07@users.noreply.github.com>
2022-08-11 19:42:43 +0530

    Create (USACO)-Angry Cows.cpp

---
+++ (USACO)-Angry Cows.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define nl "\n"
+#define all(x) (x).begin(), (x).end()
+
+const int N = 5e4+5;
+int n, m;
+vector<int> v(N);
+
+bool valid(int mid){
+  int k=0, dist=0;
+  for(int i=0; i<n; i++){
+    if(v[i]>dist){
+      dist=v[i]+mid*2;
+      k++;
+    }
+  }
+  if(k>m) return false;
+  return true;
+}
+
+int main() {
+  #ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+  #endif
+
+  ios_base::sync_with_stdio(false);
+  cout.tie(nullptr);
+
+if(fopen("angry.in", "r")){
+  freopen("angry.in","r",stdin);
+  freopen("angry.out","w",stdout);
+}
+  cin>>n>>m;
+  for(int i=0; i<n; i++)
+    cin>>v[i];
+
+  v.resize(n);
+  sort(all(v));
+
+  int lo=0, hi=v[n-1];
+  while(hi-lo>1){
+    int mid = (lo+hi)/2;
+    if(valid(mid)) hi=mid;
+    else lo=mid+1;
+  }
+
+  if(valid(lo)) cout<<lo;
+  else cout<<hi;
+}
