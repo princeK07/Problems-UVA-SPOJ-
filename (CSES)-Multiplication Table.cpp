7779b866d1f9c71e7e4ddfe28f51ed2fe4bb2617
l3vel <109853601+princeK07@users.noreply.github.com>
2022-08-16 11:32:33 +0530

    Create (CSES)-Multiplication Table.cpp

---
+++ (CSES)-Multiplication Table.cpp
@@ -0,0 +1,43 @@
+/*
+  Time Complexity: O(log(n)*n)
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define nl "\n"
+typedef long long ll;
+
+void solve(){
+  ll n; cin>>n;
+  ll lo=1, hi=n*n;
+
+  ll req_pos = (n*n + 1)/2;
+
+  while(hi>lo){
+    ll mid = (lo+hi)/2;
+    ll curr=0;
+
+    for(int i=1; i<=n; i++){
+      curr+=min(n, mid/i);
+    }
+
+    if(curr>=req_pos) hi=mid;
+    else lo=mid+1;
+  }
+  cout<<hi;
+}
+
+int main() {
+
+  ios_base::sync_with_stdio(false);
+  cout.tie(nullptr);
+
+  int t=1;
+  // cin>>t;
+  while(t--){
+    solve();
+    // cout<<nl;
+  }
+
+}
